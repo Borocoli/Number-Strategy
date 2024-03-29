130835c1ae9b4ac0eb1daca3e864dee4bc258940
Broccoli <42372902+Borocoli@users.noreply.github.com>
2024-03-29 11:57:34 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,12 +7,16 @@ public class Main {
 	int[] set1 = new int[]{3, 5, 7};
 	int[] set2 = new int[]{4, 6, 8};
 	
-	NPC n1 = new NPC(set1);
+	Human n1 = new Human(set1);
 	NPC n2 = new NPC(set2);
 
 	int[] move;
 	int win;
-
+	System.out.println("Your values are:");
+	for(int e : set1) {
+		System.out.print(e);
+		System.out.print(" ");
+	System.out.println();
 	while(true) {
 		//table.print();
 		move = n1.play(table);
@@ -26,7 +30,8 @@ public class Main {
 			System.out.println("Player 1 loses");
 			break;
 		}
-	
+		System.out.println();
+
 		move = n2.play(table);
 		System.out.println("Player 2 plays");
 		win = table.move(move[0], (char)(move[1] + 'a'), move[2]);
@@ -38,6 +43,8 @@ public class Main {
 			System.out.println("Player 2 loses");
 			break;
 		}
+		System.out.println();
+
 
 	}
 
